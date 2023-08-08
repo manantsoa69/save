c105f36adebb1a749097d342808d67ce0ae54461
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-08-08 20:39:04 +0200

    Update saveSubscription.js

--- helper/saveSubscription.js
+++ helper/saveSubscription.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql2/promise');
 const Redis = require('ioredis');
 require('dotenv').config();
-
+// Connect to the second Redis account
 const redis = new Redis(process.env.REDIS_URL);
-console.log('Connected to redis');
+console.log('Connected to Redis');
+// Connect to the second Redis account
+const redis1 = new Redis(process.env.REDIS_URL1);
+console.log('Connected to Redis 1');
 const pool = mysql.createPool(process.env.DATABASE_URL);
 console.log('Connected to mysql');
 
@@ -27,7 +30,7 @@ const saveSubscription = async (fbid, subscriptionStatus) => {
     const connection = await pool.getConnection();
     try {
       await connection.query('UPDATE users SET expireDate = ? WHERE fbid = ?', [expireDate.toISOString(), fbid]);
-      console.log('Subscription expiration date updated in MySQL:', subscriptionStatus);
+      console.log('Subscription expiration date updated in MySQL:', subscriptionStatus);   
     } finally {
       connection.release();
     }
@@ -35,7 +38,14 @@ const saveSubscription = async (fbid, subscriptionStatus) => {
     // Update the expiration date in Redis as well
     const cacheKey = `${fbid}`;
     const expireDateInSeconds = Math.ceil((new Date(expireDate) - new Date()) / 1000);
-    await redis.setex(cacheKey, expireDateInSeconds, expireDate.toISOString());
+    const updatePromises = [
+      redis.setex(cacheKey, expireDateInSeconds, expireDate.toISOString()),
+      redis1.setex(cacheKey, expireDateInSeconds, expireDate.toISOString())
+    ];
+
+    // Execute both updates simultaneously using Promise.all
+    await Promise.all(updatePromises);
+    console.log('Subscription expiration date updated in Redis:', subscriptionStatus);
 
     return true;
   } catch (error) {
