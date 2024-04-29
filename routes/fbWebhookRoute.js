13cb80c6400cc570cd0f2352f87e2691df13dc41
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2024-04-29 02:31:21 +0200

    Add files via upload

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -1,4 +1,3 @@
-// routes/fbWebhookRoute.js
 const express = require('express');
 const router = express.Router();
 const { checkSubscription, check } = require('../helper/subscriptionHelper');
@@ -12,11 +11,8 @@ const { transletResponse } = require('../helper/sendPost');
 const { saveParams, saveChatHistory, googleBooksAPI } = require('./manageRoote');
 const { saveSubscription } = require('../helper/saveSubscription');
 
-// Store the last processed prompts in an object
-
 const lastProcessedPrompts = {};
 
-// Shuffle function to shuffle the array containing function names
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -41,18 +37,18 @@ router.post('/', async (req, res) => {
 
     const { text: query } = message;
     console.log(`fbid ${fbid}msg${query}`);
-//Number cheak
+
     if (/^03\d+$/.test(query)) {
       const numberValidationResult = await checkNumber(query, fbid);
       await sendMessage(fbid, numberValidationResult);
       console.log('Number message sent:', numberValidationResult);
       return res.sendStatus(200);
     }
-//Quick reply endlear
+
     if (message.quick_reply && message.quick_reply.payload) {
       const payload = message.quick_reply.payload;
       console.log(`payload ${payload}`);
-// translet the bot answer
+
       if (payload.trim().startsWith('/')) {
         const comment = payload.trim().substring(1);
         const { access, chatHistory } = await check(fbid);
@@ -60,18 +56,21 @@ router.post('/', async (req, res) => {
           const chatAfterAIIndex = chatHistory.indexOf('AI:');
           if (chatAfterAIIndex !== -1) {
             const lastPrompt = chatHistory.substring(chatAfterAIIndex + 3).trim();
- 
             result = await transletResponse(fbid, lastPrompt, comment);
             await sendMessage(fbid, result);
             return res.sendStatus(200);
           }
         }
       } else if (['Chat', 'Trad', 'Live', 'Book'].includes(payload)) {
-        await saveParams(fbid, payload);
-        await sendMessage(fbid, `Vous êtes en mode ${payload} GPT. 🔥`);
-        if (payload === 'Book') {
-          await sendMessage(fbid, "📚 Envoyez simplement le titre de votre livre.")
-
+        const { Status } = await checkSubscription(fbid);
+        if (Status === 'EE' || Status === 'E') {
+          
+        } else {
+          await saveParams(fbid, payload);
+          await sendMessage(fbid, `Vous êtes en mode ${payload} GPT. 🔥`);
+          if (payload === 'Book') {
+            await sendMessage(fbid, "📚 Envoyez simplement le titre de votre livre.");
+          }
         }
         return res.sendStatus(200);
       } else if (payload === 'oui') { 
@@ -83,21 +82,14 @@ router.post('/', async (req, res) => {
         // Handle the action for this payload
         return res.sendStatus(200);
       } else if (payload === 'tuto') {
-        const postUrl = "Cliquez sur ce lien pour accéder au tutoriel : https://fb.watch/rIsweu7vLZ/"; 
+        const postUrl = "Cliquez sur ce lien pour accéder au tutoriel : https://www.facebook.com/yourpage/posts/1234567890"; 
         // Send the post URL as a message to the user
         await sendMessage(fbid, postUrl);
-        
-        // Handle the action for this payload
-        return res.sendStatus(200);
-      } else {
-        const lastPrompt = lastProcessedPrompts[fbid] || 'unknown';
-        result = await transletResponse(fbid, lastPrompt, payload);
 
-        await sendMessage(fbid, result);
+        // Handle the action for this payload
         return res.sendStatus(200);
       }
     }
-
 
     if (query.toLowerCase() === 'trad') {
       await selectLangtrad(fbid);
@@ -109,33 +101,24 @@ router.post('/', async (req, res) => {
       return res.sendStatus(200);
     }
 
-    //if (lastProcessedPrompts[fbid] === query) {
-      //console.log('ignoring new request.');
-      //return res.sendStatus(200);
-    //}
-
     lastProcessedPrompts[fbid] = query;
 
     const { Status, chathistory } = await checkSubscription(fbid);
 
-    if (Status === 'C') { //CHAT BOT
-      const functions = shuffle([googlechat, googlechat1, googlechat2, googlechat3]);
-
+    if (Status === 'C') { 
+      const functions = shuffle([googlechat3]);
       let result;
       for (const func of functions) {
         result = await func(chathistory, query, 'Chat');
         if (result) break;
       }
-      
       await Promise.all([
         saveChatHistory(fbid, query, result),
         sendMessage(fbid, result),
       ]);
-    } else if (Status === 'B') { //BOOK
+    } else if (Status === 'B') { 
       const functions = shuffle([googlechat, googlechat1, googlechat2, googlechat3]);
-
       const aiResult = await googleBooksAPI(query);
-     
       if (aiResult === null) {
         await sendMessage(fbid, "Je n'ai trouvé aucune information sur votre livre. Veuillez fournir à nouveau le titre du livre.");
         return res.sendStatus(200);
@@ -147,32 +130,26 @@ router.post('/', async (req, res) => {
           if (result) break;
         }
         if (result) {
-          
-          await  sendMessage(fbid, result);
-
+          await sendMessage(fbid, result);
         } else {
           await sendMessage(fbid, "I couldn't process your request. Please try again later.");
         }
       }
-
-    } else if (Status === 'Q') {
+    } else if (Status === 'Q') { 
       const result = await googlefirst(query);
       if (result && result.content) {
         const responseText = result.content;
         await sendMessage(fbid, responseText);
       }
-    } else if (Status === 'T') { //TRAD GPT
+    } else if (Status === 'T') { 
       await selectLang(fbid);
-    } else if (Status === 'L') { //LIVE GPT
- 
-      const functions = shuffle([googlechat, googlechat1, googlechat2, googlechat3]);
-
+    } else if (Status === 'L') { 
+      const functions = shuffle([ googlechat3]);
       let result;
       for (const func of functions) {
         result = await func(chathistory, query, 'Live');
         if (result) break;
       }
-      
       await Promise.all([
         saveChatHistory(fbid, query, result),
         sendMessage(fbid, result),
@@ -183,12 +160,10 @@ router.post('/', async (req, res) => {
 
   } catch (error) {
     console.error('Error occurred:', error);
-    //if (fbid) {
-      //delete lastProcessedPrompts[fbid];
-   // }
     res.sendStatus(500);
   }
 });
+
 
 router.get('/', (req, res) => {
   const mode = req.query['hub.mode'];
